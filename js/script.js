11bbf5f63ca1b9ee9abc2f091370f71556810a0a
Tommaso Passarotto <passarotto13@gmail.com>
2021-11-04 15:54:18 +0100

    pseudocodice

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// 1. creo un array di oggetti in cui metto ogni membro con le relative descrizioni
+// 2. stampo tutti i membri nello html
+// 3. prendo i dati dall'input per aggiungere nuovi membri
+
