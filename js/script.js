5576a1f250c44f683bf678bab46e8642c18936f7
Tommaso Passarotto <passarotto13@gmail.com>
2021-11-04 16:50:25 +0100

    tentativo di aggiunta ciclo per i membri

--- js/script.js
+++ js/script.js
@@ -4,37 +4,37 @@
 
 const members = [
     {
-        "img"  : <img src="img\wayne-barnett-founder-ceo.jpg" alt="Wayne Barnett"/>,
+        "img"  : "<img src=`img\wayne-barnett-founder-ceo.jpg` alt=`Wayne Barnett`/>",
         "name" : "Wayne Barnett",
         "role" : "Founder & CEO"
     },
 
     {
-        "img"  : <img src=".img\angela-caroll-chief-editor.jpg" alt="Angel Caroll"/>,
+        "img"  : "<img src=`img\angela-caroll-chief-editor.jpg` alt=`Angel Caroll`/>",
         "name" : "Angel Caroll",
         "role" : "Chief Editor"
     },
 
     {
-        "img"  : <img src="img\walter-gordon-office-manager.jpg" alt="Walter Gordon"/>,
+        "img"  : "<img src=`img\walter-gordon-office-manager.jpg` alt=`Walter Gordon`/>",
         "name" : "Walter Gordon",
         "role" : "Office Manager"
     },
 
     {
-        "img"  : <img src="img\angela-lopez-social-media-manager.jpg" alt="Angela Lopez"/>,
+        "img"  : "<img src=`img\angela-lopez-social-media-manager.jpg` alt=`Angela Lopez`/>",
         "name" : "Angela Lopez",
         "role" : "Social Media Manager"
     },
 
     {
-        "img"  : <img src="img\scott-estrada-developer.jpg" alt="Scott Estrada"/>,
+        "img"  : "<img src=`img\scott-estrada-developer.jpg` alt=`Scott Estrada`/>",
         "name" : "Scott Estrada",
         "role" : "Developer"
     },
 
     {
-        "img"  : <img src="img\barbara-ramos-graphic-designer.jpg" alt="Barbara Ramos"/>,
+        "img"  : "<img src=`img\barbara-ramos-graphic-designer.jpg` alt=`Barbara Ramos`/>",
         "name" : "Barbara Ramos",
         "role" : "Graphic Designer"
     }
@@ -44,28 +44,42 @@ const members = [
 // contenitore dei membri
 const memberCont = document.getElementsByClassName("team-container");
 
-// stampo i membri già presenti dall'array all'html
-// creo i div per ogni membro
-let memberImg = document.createElement("div");
-memberImg.classList.add("card-image");
-memberPage.append(memberImg);
+// stampo i membri già presenti dall'array all'html con un ciclo
 
-let memberText = document.createElement("div");
-memberText.classList.add("card-text");
-memberPage.append(memberText);
+for (i = 0; i < 6; i++) {
+    // creo il div per gli elementi
+    let memberPage = document.createElement("div");
+    memberPage.classList.add("team-card");
+    memberPage.append(i);
+    memberCont.append(memberPage);
 
-// creo il div per gli elementi
-let memberPage = document.createElement("div");
-memberPage.classList.add("team-card");
-memberCont.append(memberPage);
+    // creo i div per ogni membro
+    let memberImg = document.createElement("div");
+    memberImg.classList.add("card-image");
+    memberPage.append(memberImg);
 
+    let memberText = document.createElement("div");
+    memberText.classList.add("card-text");
+    memberPage.append(memberText);
+}
 
 // aggiungo i pezzi dell'array nei div giusti
+for (let img in members) {
+    memberImg.append(members[img]);
+}
+
+for (let name in members) {
+    memberText.append(members[name]);
+}
+
+for (let role in members) {
+    memberText.append(members[role]);
+}
 // memberImg.innerHTML = members.img;
 // memberText.innerHTML = members.name;
 // memberText.innerHTML = members.role;
-// ...
-// memberImg.append(members.img);
-// memberText.append(members.name);
-// memberText.append(members.role);
+
+
+
+
 
