53a9e867943cb5e2a57e7ed1360c9a530bcbba17
Tommaso Passarotto <passarotto13@gmail.com>
2021-11-04 16:04:34 +0100

    array object con membri già presenti

--- js/script.js
+++ js/script.js
@@ -2,3 +2,40 @@
 // 2. stampo tutti i membri nello html
 // 3. prendo i dati dall'input per aggiungere nuovi membri
 
+const members = [
+    {
+        "img"  : <img src="img\wayne-barnett-founder-ceo.jpg" alt="Wayne Barnett"/>,
+        "name" : "Wayne Barnett",
+        "role" : "Founder & CEO"
+    },
+
+    {
+        "img"  : <img src=".img\angela-caroll-chief-editor.jpg" alt="Angel Caroll"/>,
+        "name" : "Angel Caroll",
+        "role" : "Chief Editor"
+    },
+
+    {
+        "img"  : <img src="img\walter-gordon-office-manager.jpg" alt="Walter Gordon"/>,
+        "name" : "Walter Gordon",
+        "role" : "Office Manager"
+    },
+
+    {
+        "img"  : <img src="img\angela-lopez-social-media-manager.jpg" alt="Angela Lopez"/>,
+        "name" : "Angela Lopez",
+        "role" : "Social Media Manager"
+    },
+
+    {
+        "img"  : <img src="img\scott-estrada-developer.jpg" alt="Scott Estrada"/>,
+        "name" : "Scott Estrada",
+        "role" : "Developer"
+    },
+
+    {
+        "img"  : <img src="img\barbara-ramos-graphic-designer.jpg" alt="Barbara Ramos"/>,
+        "name" : "Barbara Ramos",
+        "role" : "Graphic Designer"
+    }
+]
